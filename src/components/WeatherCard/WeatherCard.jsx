5e464977981094ef77793f485a73679406057ec3
Hellnwo <hellenwo12@gmail.com>
2025-06-08 00:14:58 -0400

    Add Dynamic Weather Card

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,10 +1,25 @@
 import './WeatherCard.css'
-import cloudy from '../../assets/cloudy.png'
+import { weatherOptions, defaultWeatherOptions } from '../../utils/constants.js'
+
 
 function WeatherCard( { weatherData } ) {
+const filteredOption = weatherOptions.filter((option) => {
+    return (
+        option.day === weatherData.isDay &&
+        option.condition === weatherData.condition
+    );
+});
+
+let weatherOption;
+if (filteredOption.length === 0) {
+    weatherOption = defaultWeatherOptions[weatherData.isDay ? 'day' : 'night'];
+} else {
+    weatherOption = filteredOption[0];
+}
+
     return (
         <section classsName='weather-card'>
-            <img src={cloudy} className='weather-card__img' />
+            <img src={weatherOption.url} alt={`Card showing ${weatherOption?.day ? 'day' : 'night'} time ${weatherOption?.condition} weather`} className='weather-card__img' />
             <p className='weather-card__temp'>{weatherData.temp.F} ° </p>
         </section>
     )
