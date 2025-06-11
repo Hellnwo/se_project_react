4e4b93580eff1d41bec988b1be53499c690ad515
Hellnwo <hellenwo12@gmail.com>
2025-06-11 13:07:35 -0400

    Alt & Btn tweaks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -74,19 +74,19 @@ function App() {
         <fieldset className="modal__radio-btns">
           <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
-            <input id="hot" type="radio" className="modal__radio-input" /> Hot
+            <input id="hot" type="radio" name="choice" className="modal__radio-input" /> Hot
           </label>
           <label
             htmlFor="warm"
             className="modal__label modal__label_type_radio"
           >
-            <input id="warm" type="radio" className="modal__radio-input" /> Warm
+            <input id="warm" type="radio" name="choice" className="modal__radio-input" /> Warm
           </label>
           <label
             htmlFor="cold"
             className="modal__label modal__label_type_radio"
           >
-            <input id="cold" type="radio" className="modal__radio-input" /> Cold
+            <input id="cold" type="radio" name="choice" className="modal__radio-input" /> Cold
           </label>
         </fieldset>
       </FormModal>
